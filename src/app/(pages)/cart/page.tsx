11244f223ef480e254e1124f3252eb7d9c9c7a23
rahim <umudyan2014@gmail.com>
2023-12-01 21:04:02 +0300

    added responsive and react query

--- src/app/(pages)/cart/page.tsx
+++ src/app/(pages)/cart/page.tsx
@@ -1,7 +1,7 @@
 import { BasketTable } from '@/entities/BasketTable/BasketTable'
 import { PageHeadInfo } from '@/shared/PageHeadInfo/PageHeadInfo'
 import { BasketItems } from '@/widgets/BasketItems/BasketItems'
-import { RecommendedSectiion } from '@/widgets/RecommendedSection/RecommendedSection'
+import { RecommendedSection } from '@/widgets/RecommendedSection/RecommendedSection'
 import { SameProducts } from '@/widgets/SameProducts/SameProducts'
 import React from 'react'
 
